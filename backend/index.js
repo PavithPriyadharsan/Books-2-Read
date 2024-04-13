819c7d280dd8e476e0da6c9ed0bc79a517ce3991
PavithPriyadharsan <pavithpriyadarshan222@gmail.com>
2024-04-13 20:33:53 +0530

    New changes

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 import booksRoute from './routes/booksRoute.js';
 import cors from 'cors';
 import dotenv from 'dotenv';
-dotenv.config();
+
+dotenv.config({path:'../.env'});
 
 // Create an express app
 
@@ -14,14 +15,6 @@ app.use(express.json());
 
 // Middleware for handling CORS POLICY
 app.use(cors());
-// Option 2: Allow Custom Origins
-// app.use(
-//   cors({
-//     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type'],
-//   })
-// );
 
 app.get('/', (request, response) => {
   console.log(request);
@@ -35,7 +28,7 @@ mongoose
   .then(() => {
     console.log('App connected to database');
     app.listen(5555, () => {
-      console.log(`App is listening to port 5555`);
+      console.log(`App is listening to port`);
     });
   })
   .catch((error) => {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Spinner from '../components/Spinner';
 import { Link } from 'react-router-dom';
-import { AiOutlineEdit } from 'react-icons/ai';
-import { BsInfoCircle } from 'react-icons/bs';
-import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+import { MdOutlineAddBox } from 'react-icons/md';
 import BooksTable from '../components/home/BooksTable';
 import BooksCard from '../components/home/BooksCard';
 
@@ -28,34 +26,58 @@ const Home = () => {
   }, []);
 
   return (
-    <div className='p-4'>
-      <div className='flex justify-center items-center gap-x-4'>
-        <button
-          className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg'
-          onClick={() => setShowType('table')}
-        >
-          Table
-        </button>
-        <button
-          className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg'
-          onClick={() => setShowType('card')}
-        >
-          Card
-        </button>
+    <div className="bg-gray-100 min-h-screen">
+      <nav className="bg-blue-300 p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <div className="flex items-center gap-x-2">
+          <svg
+              className="h-6 w-6 text-white"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M9 19V5l7 7-7 7z"
+              />
+            </svg>
+            <Link to="/" className="text-black text-xl font-bold">
+              Books2Read
+            </Link>
+          </div>
+          <div className="flex justify-center items-center gap-x-4">
+            <button
+              className={`bg-sky-600 hover:bg-sky-900 px-4 py-1 rounded-lg ${showType === 'table' ? 'bg-sky-600 text-white' : ''}`}
+              onClick={() => setShowType('table')}
+            >
+              Table
+            </button>
+            <button
+              className={`bg-sky-600 hover:bg-sky-900 px-4 py-1 rounded-lg ${showType === 'card' ? 'bg-sky-600 text-white' : ''}`}
+              onClick={() => setShowType('card')}
+            >
+              Card
+            </button>
+          </div>
+        </div>
+      </nav>
+      <div className="container mx-auto p-4">
+        <div className="flex justify-between items-center mb-8">
+          <h1 className="text-3xl font-medium">Books List</h1>
+          <Link to="/books/create">
+            <MdOutlineAddBox className="text-sky-800 text-4xl" />
+          </Link>
+        </div>
+        {loading ? (
+          <Spinner />
+        ) : showType === 'table' ? (
+          <BooksTable books={books} />
+        ) : (
+          <BooksCard books={books} />
+        )}
       </div>
-      <div className='flex justify-between items-center'>
-        <h1 className='text-3xl my-8'>Books List</h1>
-        <Link to='/books/create'>
-          <MdOutlineAddBox className='text-sky-800 text-4xl' />
-        </Link>
-      </div>
-      {loading ? (
-        <Spinner />
-      ) : showType === 'table' ? (
-        <BooksTable books={books} />
-      ) : (
-        <BooksCard books={books} />
-      )}
     </div>
   );
 };
